962ee00bc20cb5ebc6013866d4e0a55318a18450
Steven Hernandez Rios <estevenhr@hotmail.com>
2017-01-24 09:46:16 -0500

    Modelo Observacion

---
+++ app/Observacion.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Observacion extends Model
+{
+    //
+
+    protected $table = 'observacion';
+	protected $primaryKey = 'id';
+	protected $fillable = ['id_registro','id_persona','observacion','estado'];
+	protected $connection = ''; 
+	public $timestamps = false;
+
+
+    public function paa()
+    {
+        return $this->belongsTo('App\Paa','id_registro');
+    }
+}
